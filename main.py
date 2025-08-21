aedc044f413f8f2cc724d5c60e458527d2f1308f
ねこち <150504981+nekotidayo222@users.noreply.github.com>
2025-08-22 07:41:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ async def draw(ctx, *, text: str):
     d = ImageDraw.Draw(img)
     # フォント指定（エラー時は標準フォント）
     try:
-    font = ImageFont.truetype("NotoSansJP-Regular.otf", 40)  # ←インデントあり
+        font = ImageFont.truetype("NotoSansJP-Regular.otf", 40)  # ←インデントあり
     except:
-    font = ImageFont.load_default()
+        font = ImageFont.load_default()
     d.text((10, 80), text, fill=(0, 0, 0), font=font)
     
     # 画像をバイト配列に
